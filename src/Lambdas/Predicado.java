14fa69f79c35dced45603e248cfbb7d75bc108a7
yonara ferreira <yonara.fd@gmail.com>
2023-01-17 22:38:04 -0300

    aula 180- aprendendo a interface Predicate

---
+++ src/Lambdas/Predicado.java
@@ -0,0 +1,15 @@
+package Lambdas;
+
+import java.util.function.Predicate;
+
+public class Predicado {
+
+	public static void main(String[] args) {
+		
+		Predicate<Produto> isCaro = 
+				prod -> (prod.preco * (1 - prod.desconto)) >= 750;
+				
+		Produto produtor = new Produto("notebook gamer", 5562.99, 0.15);
+			System.out.println(isCaro.test(produtor));
+	}
+}
