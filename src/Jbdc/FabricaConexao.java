c08f5ee8c7b300005297679eaf9798602b4f0550
yonara ferreira <yonara.fd@gmail.com>
2023-03-08 23:01:35 -0300

    Criando tabelas e inserindo registros

--- src/Jbdc/FabricaConexao.java
+++ src/Jbdc/FabricaConexao.java
@@ -9,7 +9,7 @@ public class FabricaConexao {
 	public static Connection getConexao() {
 		
 		try {
-			final String url = "jdbc:mysql://localhost?verifyServerCertificate=false&useSSL=false";
+			final String url = "jdbc:mysql://localhost/curso_java?verifyServerCertificate=false&useSSL=false";
 			final String usuario = "root";
 			final String senha = "root";
 			
